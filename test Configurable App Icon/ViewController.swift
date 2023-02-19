82acbc895906de94f910250f0ebf53d662a2c352
iSlam AbdelAziz <i.abdelaziz.d@gmail.com>
2023-02-19 20:14:22 +0200

    Initial Commit

---
+++ test Configurable App Icon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test Configurable App Icon
+//
+//  Created by iSlam AbdelAziz on 19/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
